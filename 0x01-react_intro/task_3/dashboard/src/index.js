d7b35dd09166cc9ace30b857d38aca70a87f49b0
Ayanwales <Ayanwalesulaymon@gmail.com>
2023-11-06 15:31:29 +0100

    utils

---
+++ 0x01-react_intro/task_3/dashboard/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Notifications from './Notifications';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <div className="root-notifications">
+      <Notifications />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
